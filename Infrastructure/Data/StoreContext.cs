7dc242934d106fbf306035e05a6a18f3ac5ecf55
bgani <beisembayev.gk@gmail.com>
2020-12-12 17:17:23 +0600

    API Sorting. Working around the decimal problem in Sqlite

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,22 @@ namespace Infrastructure.Data
             base.OnModelCreating(modelBuilder);
             // specifying an assembly where we have configs
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // converting decimal to double in Sqlite when writing and reading
+            if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties()
+                        .Where(p => p.PropertyType == typeof(decimal));
+                    
+                    foreach(var prperty in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(prperty.Name)
+                            .HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
