b89363c940ab1f3531c948974ac925884735b3e0
bgani <beisembayev.gk@gmail.com>
2020-12-15 15:05:22 +0600

    Client UI. Consuming services in the shop component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,18 +10,9 @@ import { IProduct } from './shared/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[] = [];
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http
-      .get<IPagination>('https://localhost:5001/api/products?pageSize=50')
-      .subscribe((response: IPagination) => {
-        this.products = response.data;
-
-      },
-      error => {
-        console.log(error);
-      });
+    
   }
 }
