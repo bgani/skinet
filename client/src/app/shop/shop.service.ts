1a98e29c54f4f53503d7be3a2e5748d3c4b27547
bgani <beisembayev.gk@gmail.com>
2020-12-15 14:55:26 +0600

    Client UI. Injecting HttpClient into shop service

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getProducts(){
+    return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
 }
