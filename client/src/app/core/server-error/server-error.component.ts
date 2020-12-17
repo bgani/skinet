f2730b9dfa823a98d7c62534e137406ef1af2635
bgani <beisembayev.gk@gmail.com>
2020-12-17 14:57:09 +0600

    Client - Error handling. Improving the internal server error component

--- client/src/app/core/server-error/server-error.component.ts
+++ client/src/app/core/server-error/server-error.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
   templateUrl: './server-error.component.html',
-  styleUrls: ['./server-error.component.scss']
+  styleUrls: ['./server-error.component.scss'],
 })
 export class ServerErrorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  error: any;
+  // navigationExtras only available in the constructor, in ngOnInit it will be undefined
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    // be defensive
+    this.error =
+      navigation &&
+      navigation.extras &&
+      navigation.extras.state &&
+      navigation.extras.state.error;
   }
 
+  ngOnInit(): void {}
 }
