ac293bbee7e1ecc0c88298adfdd45f340dac6a5b
bgani <beisembayev.gk@gmail.com>
2020-12-20 22:24:23 +0600

    Client - Basket. Adding the basket component functionality quantity increment, decrement, remove

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -63,7 +63,7 @@ export class BasketService {
     const basket = this.getCurrentBasketValue();
     const foundItemIndex = basket.items.findIndex(x => x.id === item.id);
     if(basket.items[foundItemIndex].quantity > 1){
-      basket.items.[foundItemIndex].quantity--;
+      basket.items[foundItemIndex].quantity--;
       this.setBasket(basket);
     } else {
       this.removeItemFromBasket(item);
