61f504506c11901552e034038a1722a65cdf6012
bgani <beisembayev.gk@gmail.com>
2020-12-21 20:35:10 +0600

    API - Identity. Adding the IdentityDbContext

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.MIddleware;
 using AutoMapper;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +35,10 @@ namespace API
             // once the request is finished, then the StoreContext is disposed
             services.AddDbContext<StoreContext>(x =>
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            
+            services.AddDbContext<AppIdentityDbContext>(x => {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"))
+            });
 
             services.AddSingleton<IConnectionMultiplexer>(c => {
                 var configuration =  ConfigurationOptions
