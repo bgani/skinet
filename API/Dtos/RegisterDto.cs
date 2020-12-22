d50c7999567fd9137397332daa2ee56e184a55ee
bgani <beisembayev.gk@gmail.com>
2020-12-22 21:32:23 +0600

    API - Validation. Checking for duplicate email addresses, adding regular expression for password validation

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        // The regex from https://regexlib.com
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alhpanumeric and at least 6 characters")]
         public string Password { get; set; }
         
     }
