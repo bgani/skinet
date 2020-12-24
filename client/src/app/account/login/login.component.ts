4b1978bf890624486b82ef0f05703c5966065b58
bgani <beisembayev.gk@gmail.com>
2020-12-24 14:32:27 +0600

    Client - Identity. Submitting data to the server

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,29 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup = null as any;
 
-  constructor() { }
- 
+  constructor(private accountService: AccountService) {}
+
   ngOnInit(): void {
     this.createLoginForm();
   }
 
-  createLoginForm(){
+  createLoginForm() {
     this.loginForm = new FormGroup({
       email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
+      password: new FormControl('', Validators.required),
     });
   }
 
-  onSubmit(){
-    console.log(this.loginForm.value);
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe(
+      () => {
+        console.log('user logged in');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
