f436f04517d31bec38ad98886f99109ba4e33b20
bgani <beisembayev.gk@gmail.com>
2020-12-24 22:15:53 +0600

    Client - Identity. Async validation, check if email exists on registering. Using AsyncValidatorFn, switchMap, timer & of

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AsyncValidatorFn,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { of, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { AccountService } from '../account.service';
 
 @Component({
@@ -22,6 +29,7 @@ export class RegisterComponent implements OnInit {
     this.createRegisterForm();
   }
 
+  // async validators only called if sync validators have passed validation
   createRegisterForm() {
     this.registerForm = this.fb.group({
       displayName: [null, [Validators.required]],
@@ -31,6 +39,9 @@ export class RegisterComponent implements OnInit {
           Validators.required,
           Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
         ],
+        [
+          this.validateEmailNotTaken()
+        ]
       ],
       password: [null, Validators.required],
     });
@@ -46,5 +57,22 @@ export class RegisterComponent implements OnInit {
         this.errors = error.errors;
       }
     );
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control) => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if (!control.value) {
+            return of(null);
+          }
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map((res) => {
+              return res ? { emailExists: true } : null;
+            })
+          );
+        })
+      );
+    };
   }
 }
