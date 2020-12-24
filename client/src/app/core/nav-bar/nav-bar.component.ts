70192d63097678b40c5ce88f86bb239eb09e34a6
bgani <beisembayev.gk@gmail.com>
2020-12-24 14:41:07 +0600

    Client - Identity. Updating the nav bar for logged in user

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
+import { IUser } from 'src/app/shared/models/user';
 
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'] 
 })
 export class NavBarComponent implements OnInit {
-
-  constructor(private basketService: BasketService) { }
   // async pipe will dispose thise observable for us
   basket$: Observable<IBasket> = new Observable<IBasket>();
+  currentUser$:  Observable<IUser> = null as any;
+
+  constructor(private basketService: BasketService, private accountSerivice: AccountService) { }
+
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.currentUser$ = this.accountSerivice.currentUser$;
   }
 
 }
