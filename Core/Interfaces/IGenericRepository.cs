9a18225de2bf4d7dffff6cb3edc62ed851a636be
bgani <beisembayev.gk@gmail.com>
2020-12-26 12:30:06 +0600

    API - Orders. Updateing the generic repository

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -5,6 +5,8 @@ using Core.Specifications;
 
 namespace Core.Interfaces
 {
+    // non of the methods are async, the reason is we're not gonna be directly adding to database when we use any of these methods
+    // The repository is not responsible for saving changes to the database, that's left to Uow
     public interface IGenericRepository<T> where T: BaseEntity
     {
         Task<T> GetByIdAsync(int id);
@@ -12,5 +14,9 @@ namespace Core.Interfaces
         Task<T> GetEntityWithSpec(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
         Task<int> CountAsync(ISpecification<T> spec);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
     }
 }
