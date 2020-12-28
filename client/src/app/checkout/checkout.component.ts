5a94b6ba4aee87a09a130b40f3338fa3542f1bfa
bgani <beisembayev.gk@gmail.com>
2020-12-28 22:39:52 +0600

    Client - Checkout. Populationg the address form from API

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
-  checkoutForm: FormGroup = null as any; 
-  constructor(private fb: FormBuilder) { }
+  checkoutForm: FormGroup = null as any;
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.createCheckoutForm();
+    this.getAddressFormValues();
   }
 
-  createCheckoutForm(){
+  createCheckoutForm() {
     this.checkoutForm = this.fb.group({
       addressForm: this.fb.group({
         firstName: [null, Validators.required],
@@ -25,13 +30,24 @@ export class CheckoutComponent implements OnInit {
         zipcode: [null, Validators.required],
       }),
       deliveryForm: this.fb.group({
-        deliveryMethod: [null, Validators.required]
+        deliveryMethod: [null, Validators.required],
       }),
       paymentFrom: this.fb.group({
-        nameOnCard: [null, Validators.required]
-      })
+        nameOnCard: [null, Validators.required],
+      }),
     });
   }
 
-
+  getAddressFormValues() {
+    this.accountService.getUserAddress().subscribe(
+      (address) => {
+        if (address) {
+          this.checkoutForm.get('addressForm')?.patchValue(address);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
