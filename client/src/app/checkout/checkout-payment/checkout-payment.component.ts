46902e81b14856d36b9c1c426b615441f8f92d4c
bgani <beisembayev.gk@gmail.com>
2020-12-28 23:30:43 +0600

    Client - Checkout. Checkout success page

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,6 +1,7 @@
 import { templateJitUrl } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
@@ -18,7 +19,8 @@ export class CheckoutPaymentComponent implements OnInit {
   constructor(
     private basketService: BasketService,
     private checkoutService: CheckoutService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -30,7 +32,8 @@ export class CheckoutPaymentComponent implements OnInit {
       .subscribe((order: IOrder) => {
           this.toastr.success('Order created successfully');
           this.basketService.deleteLocalBasket(basket.id);
-          console.log(order);
+          const navigationExtras: NavigationExtras =  { state: order};
+          this.router.navigate(['checkout/success'], navigationExtras);
       },
       error => {
         this.toastr.error(error.message);
