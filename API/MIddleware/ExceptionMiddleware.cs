de28dab4328092bb0849a4df38b71928fda4a487
bgani <beisembayev.gk@gmail.com>
2021-01-01 18:05:29 +0600

    Publishing. Pre deployment work. Configuring seed files directory. Temporarily enhancing exception details for production

--- API/MIddleware/ExceptionMiddleware.cs
+++ API/MIddleware/ExceptionMiddleware.cs
@@ -14,7 +14,7 @@ namespace API.MIddleware
         private readonly IHostEnvironment _env;
         private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly RequestDelegate _next;
-        
+
         public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
         {
             _env = env;
@@ -22,7 +22,8 @@ namespace API.MIddleware
             _next = next;
         }
 
-        public async Task InvokeAsync(HttpContext context) {
+        public async Task InvokeAsync(HttpContext context)
+        {
             try
             {
                 // if there is no exception then the request moves on to its next stage
@@ -33,17 +34,19 @@ namespace API.MIddleware
                 _logger.LogError(ex, ex.Message);
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                
+
                 var response = _env.IsDevelopment()
                     ? new ApiException(
-                        (int)HttpStatusCode.InternalServerError, 
-                        ex.Message, 
+                        (int)HttpStatusCode.InternalServerError,
+                        ex.Message,
                         ex.StackTrace.ToString())
-                    : new ApiException((int)HttpStatusCode.InternalServerError);
-                
-                var options = new JsonSerializerOptions 
-                { 
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase 
+                    : new ApiException((int)HttpStatusCode.InternalServerError, 
+                        ex.Message,
+                        ex.StackTrace.ToString()); // exception details must be only for dev mode, remove it and redeploy after successful deployment.
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
                 };
 
                 var json = JsonSerializer.Serialize(response, options);
